ac39e962bb297654c1f69efd31baa023229e5446
fzxx <53783792+fzxx@users.noreply.github.com>
2026-06-18 12:26:19 +0800

    Fix HKDF-BLAKE2sp

--- CPP/7zip/Crypto/HkdfBlake2sp.cpp
+++ CPP/7zip/Crypto/HkdfBlake2sp.cpp
@@ -11,10 +11,14 @@ namespace NHkdfBlake2sp {
 
 #define BLAKE2SP_BLOCK_SIZE 64
 
-// HMAC-BLAKE2sp
-static void HmacBlake2sp(const Byte *key, unsigned keySize,
-    const Byte *message, unsigned messageSize,
-    Byte *mac)
+static void CloneBlake2spState(CBlake2sp *dest, const CBlake2sp *src)
+{
+  memcpy(dest, src, sizeof(CBlake2sp));
+}
+
+void Derive(const Byte *prk, unsigned prkSize,
+    const char *info, unsigned infoLen,
+    Byte *output, unsigned outSize)
 {
   static bool blake2spPrepared = false;
   if (!blake2spPrepared)
@@ -23,48 +27,50 @@ static void HmacBlake2sp(const Byte *key, unsigned keySize,
     blake2spPrepared = true;
   }
 
-  Byte ipad[BLAKE2SP_BLOCK_SIZE];
-  Byte opad[BLAKE2SP_BLOCK_SIZE];
+  Byte processedKey[Z7_BLAKE2S_DIGEST_SIZE];
+  const Byte *effectiveKey;
+  unsigned effectiveKeySize;
 
-  memset(ipad, 0x36, BLAKE2SP_BLOCK_SIZE);
-  memset(opad, 0x5c, BLAKE2SP_BLOCK_SIZE);
-
-  for (unsigned i = 0; i < keySize && i < BLAKE2SP_BLOCK_SIZE; i++)
+  if (prkSize > BLAKE2SP_BLOCK_SIZE)
   {
-    ipad[i] ^= key[i];
-    opad[i] ^= key[i];
+    CAlignedBuffer1 bufHash(sizeof(CBlake2sp));
+    CBlake2sp *blake2spHash = (CBlake2sp *)(void *)(Byte *)bufHash;
+    Blake2sp_Init(blake2spHash);
+    Blake2sp_SetFunction(blake2spHash, 0);
+    Blake2sp_Update(blake2spHash, prk, prkSize);
+    Blake2sp_Final(blake2spHash, processedKey);
+
+    effectiveKey = processedKey;
+    effectiveKeySize = Z7_BLAKE2S_DIGEST_SIZE;
+  }
+  else
+  {
+    effectiveKey = prk;
+    effectiveKeySize = prkSize;
   }
 
-  // Inner hash
-  CAlignedBuffer1 bufInner(sizeof(CBlake2sp));
-  CBlake2sp *blake2spInner = (CBlake2sp *)(void *)(Byte *)bufInner;
-  Blake2sp_Init(blake2spInner);
-  Blake2sp_SetFunction(blake2spInner, 0);
-  Blake2sp_Update(blake2spInner, ipad, BLAKE2SP_BLOCK_SIZE);
-  Blake2sp_Update(blake2spInner, message, messageSize);
+  Byte ipad[BLAKE2SP_BLOCK_SIZE];
+  Byte opad[BLAKE2SP_BLOCK_SIZE];
+  memset(ipad, 0x36, BLAKE2SP_BLOCK_SIZE);
+  memset(opad, 0x5c, BLAKE2SP_BLOCK_SIZE);
+  for (unsigned i = 0; i < effectiveKeySize; i++)
+  {
+    ipad[i] ^= effectiveKey[i];
+    opad[i] ^= effectiveKey[i];
+  }
 
-  Byte innerHash[Z7_BLAKE2S_DIGEST_SIZE];
-  Blake2sp_Final(blake2spInner, innerHash);
+  CAlignedBuffer1 bufInnerState(sizeof(CBlake2sp));
+  CBlake2sp *innerState = (CBlake2sp *)(void *)(Byte *)bufInnerState;
+  Blake2sp_Init(innerState);
+  Blake2sp_SetFunction(innerState, 0);
+  Blake2sp_Update(innerState, ipad, BLAKE2SP_BLOCK_SIZE);
 
-  // Outer hash
-  CAlignedBuffer1 bufOuter(sizeof(CBlake2sp));
-  CBlake2sp *blake2spOuter = (CBlake2sp *)(void *)(Byte *)bufOuter;
-  Blake2sp_Init(blake2spOuter);
-  Blake2sp_SetFunction(blake2spOuter, 0);
-  Blake2sp_Update(blake2spOuter, opad, BLAKE2SP_BLOCK_SIZE);
-  Blake2sp_Update(blake2spOuter, innerHash, Z7_BLAKE2S_DIGEST_SIZE);
-  Blake2sp_Final(blake2spOuter, mac);
+  CAlignedBuffer1 bufOuterState(sizeof(CBlake2sp));
+  CBlake2sp *outerState = (CBlake2sp *)(void *)(Byte *)bufOuterState;
+  Blake2sp_Init(outerState);
+  Blake2sp_SetFunction(outerState, 0);
+  Blake2sp_Update(outerState, opad, BLAKE2SP_BLOCK_SIZE);
 
-  Z7_memset_0_ARRAY(ipad);
-  Z7_memset_0_ARRAY(opad);
-  Z7_memset_0_ARRAY(innerHash);
-}
-
-// HKDF-Expand (RFC 5869)
-void Derive(const Byte *prk, unsigned prkSize,
-    const char *info, unsigned infoLen,
-    Byte *output, unsigned outSize)
-{
   const unsigned n = (outSize + Z7_BLAKE2S_DIGEST_SIZE - 1) / Z7_BLAKE2S_DIGEST_SIZE;
 
   Byte prevT[Z7_BLAKE2S_DIGEST_SIZE];
@@ -72,6 +78,11 @@ void Derive(const Byte *prk, unsigned prkSize,
 
   Byte *outPtr = output;
   unsigned remaining = outSize;
+
+  CAlignedBuffer1 bufInnerTmp(sizeof(CBlake2sp));
+  CAlignedBuffer1 bufOuterTmp(sizeof(CBlake2sp));
+  CBlake2sp *innerTmp = (CBlake2sp *)(void *)(Byte *)bufInnerTmp;
+  CBlake2sp *outerTmp = (CBlake2sp *)(void *)(Byte *)bufOuterTmp;
 
   for (unsigned i = 1; i <= n; i++)
   {
@@ -92,9 +103,16 @@ void Derive(const Byte *prk, unsigned prkSize,
 
     message[messageSize] = (Byte)i;
     messageSize += 1;
+    CloneBlake2spState(innerTmp, innerState);
+    Blake2sp_Update(innerTmp, message, messageSize);
+
+    Byte innerHash[Z7_BLAKE2S_DIGEST_SIZE];
+    Blake2sp_Final(innerTmp, innerHash);
+    CloneBlake2spState(outerTmp, outerState);
+    Blake2sp_Update(outerTmp, innerHash, Z7_BLAKE2S_DIGEST_SIZE);
 
     Byte ti[Z7_BLAKE2S_DIGEST_SIZE];
-    HmacBlake2sp(prk, prkSize, message, messageSize, ti);
+    Blake2sp_Final(outerTmp, ti);
 
     const unsigned copySize = remaining < Z7_BLAKE2S_DIGEST_SIZE ? remaining : Z7_BLAKE2S_DIGEST_SIZE;
     memcpy(outPtr, ti, copySize);
@@ -106,9 +124,13 @@ void Derive(const Byte *prk, unsigned prkSize,
 
     Z7_memset_0_ARRAY(ti);
     Z7_memset_0_ARRAY(message);
+    Z7_memset_0_ARRAY(innerHash);
   }
 
   Z7_memset_0_ARRAY(prevT);
+  Z7_memset_0_ARRAY(ipad);
+  Z7_memset_0_ARRAY(opad);
+  Z7_memset_0_ARRAY(processedKey);
 }
 
 }}
